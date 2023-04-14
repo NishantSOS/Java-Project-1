f2f67c38c5e9a3ef89b80b98603eb6ec3191c890
NishantSOS <125679080+NishantSOS@users.noreply.github.com>
2023-04-14 22:45:02 +0530

    Update project.java

--- project.java
+++ project.java
@@ -1,129 +1,145 @@
 import java.util.Scanner;
 
-class Hostel {
-    private String hostelName;
-    private int numberOfRooms;
-    private double roomPrice;
-
-    public Hostel(String hostelName, int numberOfRooms, double roomPrice) {
-        this.hostelName = hostelName;
-        this.numberOfRooms = numberOfRooms;
-        this.roomPrice = roomPrice;
-    }
-
-    public String getHostelName() {
-        return hostelName;
-    }
-
-    public void setHostelName(String hostelName) {
-        this.hostelName = hostelName;
-    }
-
-    public int getNumberOfRooms() {
-        return numberOfRooms;
-    }
-
-    public void setNumberOfRooms(int numberOfRooms) {
-        this.numberOfRooms = numberOfRooms;
-    }
-
-    public double getRoomPrice() {
-        return roomPrice;
-    }
-
-    public void setRoomPrice(double roomPrice) {
-        this.roomPrice = roomPrice;
-    }
-
-    public double calculateBill(int numberOfDays) {
-        return numberOfDays * roomPrice;
-    }
-}
-
 class Student {
-    private String studentName;
-    private String studentID;
-    private int numberOfDays;
-    private Hostel hostel;
+    String name;
+    String rollno;
+    String roomno;
+    String course;
 
-    public Student(String studentName, String studentID, int numberOfDays, Hostel hostel) {
-        this.studentName = studentName;
-        this.studentID = studentID;
-        this.numberOfDays = numberOfDays;
-        this.hostel = hostel;
+    public Student(String name, String rollno, String roomno, String course) {
+        this.name = name;
+        this.rollno = rollno;
+        this.roomno = roomno;
+        this.course = course;
     }
 
-    public String getStudentName() {
-        return studentName;
+    public String getName() {
+        return name;
     }
 
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
+    public String getRollno() {
+        return rollno;
     }
 
-    public String getStudentID() {
-        return studentID;
+    public String getRoomno() {
+        return roomno;
     }
 
-    public void setStudentID(String studentID) {
-        this.studentID = studentID;
+    public String getCourse() {
+        return course;
     }
 
-    public int getNumberOfDays() {
-        return numberOfDays;
-    }
-
-    public void setNumberOfDays(int numberOfDays) {
-        this.numberOfDays = numberOfDays;
-    }
-
-    public Hostel getHostel() {
-        return hostel;
-    }
-
-    public void setHostel(Hostel hostel) {
-        this.hostel = hostel;
-    }
-
-    public double calculateBill() {
-        return hostel.calculateBill(numberOfDays);
-    }
-
-    public String toString() {
-        return "Student Name: " + studentName + ", Student ID: " + studentID + ", Hostel: " + hostel.getHostelName() + ", Number of Days: " + numberOfDays + ", Bill: Rs." + calculateBill();
+    public void setRoomno(String roomno) {
+        this.roomno = roomno;
     }
 }
-// all the methods or class
 
-public class project {
-    public static void main(String[] args) {
-        Hostel hostel1 = new Hostel("Hostel A", 10, 50.0);
-        Hostel hostel2 = new Hostel("Hostel B", 20, 70.0);
+class Hostel {
+    Student[] students;
+    int numStudents;
+    int numRooms;
+    int[] rooms;
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter student name: ");
-        String studentName = scanner.nextLine();
-        System.out.print("Enter student ID: ");
-        String studentID = scanner.nextLine();
-        System.out.print("Enter the number of days you want to spend in hostel : ");
-        int numberOfDays = scanner.nextInt();
-        scanner.nextLine();
-        System.out.print("Enter hostel name (BH or GH): ");
-        String hostelName = scanner.nextLine();
+    public Hostel(int numStudents, int numRooms) {
+        this.numStudents = numStudents;
+        this.numRooms = numRooms;
+        this.rooms = new int[numRooms];
+        this.students = new Student[numStudents];
+    }
 
-        Hostel hostel = null;
-        if (hostelName.equalsIgnoreCase("A")) {
-            hostel = hostel1;
-        } else if (hostelName.equalsIgnoreCase("B")) {
-            hostel = hostel2;
-        } else {
-            System.out.println("Invalid hostel name");
-            System.exit(0);
+    public void addStudent(Student s) {
+        for (int i = 0; i < numStudents; i++) {
+            if (students[i] == null) {
+                students[i] = s;
+                System.out.println("Student " + s.getName() + " has been added to the system.");
+                break;
+            }
         }
+    }
 
-        Student student = new Student(studentName, studentID, numberOfDays, hostel);
-        System.out.println(student.toString());
+    public void allocateRoom() {
+        int count = 0;
+        for (int i = 0; i < numRooms; i++) {
+            if (rooms[i] == 0) {
+                count++;
+            }
+        }
+        if (count == 0) {
+            System.out.println("Sorry, there are no rooms available at the moment.");
+            return;
+        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the roll number of the student you want to allocate a room to: ");
+        String rollno = sc.nextLine();
+        for (int i = 0; i < numStudents; i++) {
+            if (students[i].getRollno().equals(rollno)) {
+                for (int j = 0; j < numRooms; j++) {
+                    if (rooms[j] == 0) {
+                        rooms[j] = 1;
+                        students[i].setRoomno(Integer.toString(j+1));
+                        System.out.println("Room " + (j+1) + " has been allocated to " + students[i].getName() + ".");
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println("Sorry, the student with roll number " + rollno + " is not registered.");
+    }
+
+    public void viewStudentDetails() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the roll number of the student: ");
+        String rollno = sc.nextLine();
+        for (int i = 0; i < numStudents; i++) {
+            if (students[i].getRollno().equals(rollno)) {
+                System.out.println("Name: " + students[i].getName());
+                System.out.println("Roll number: " + students[i].getRollno());
+                System.out.println("Course: " + students[i].getCourse());
+                System.out.println("Room number: " + students[i].getRoomno());
+                return;
+            }
+        }
+        System.out.println("Sorry, the student with roll number " + rollno + " is not registered.");
     }
 }
 
-//This is just a sample code that we wrote in small time .Further changes will be done after our mid term.
+public class HostelManagementSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of students: ");
+        int numStudents = sc.nextInt();
+        System.out.println("Enter the number of rooms: ");
+        int numRooms = sc.nextInt();
+        Hostel hostel = new Hostel(numStudents, numRooms);
+        while (true) {
+            System.out.println("Enter 1 to add a student.");
+            System.out.println("Enter 2 to allocate a room.");
+            System.out.println("Enter 3 to view student details.");
+            System.out.println("Enter 4 to exit.");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the name of the student: ");
+                    String name = sc.next();
+                    System.out.println("Enter the roll number of the student: ");
+                    String rollno = sc.next();
+                    System.out.println("Enter the course of the student: ");
+                    String course = sc.next();
+                    Student s = new Student(name, rollno, null, course);
+                    hostel.addStudent(s);
+                    break;
+                case 2:
+                    hostel.allocateRoom();
+                    break;
+                case 3:
+                    hostel.viewStudentDetails();
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        }
+    }
+}
