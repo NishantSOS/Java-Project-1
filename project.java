bcca2e111172c02a8eeb9fe2f142bcf74be9be02
NishantSOS <125679080+NishantSOS@users.noreply.github.com>
2023-04-15 17:42:57 +0530

    Update project.java

--- project.java
+++ project.java
@@ -1,144 +1,149 @@
 import java.util.Scanner;
-
-class Student {
+class Student{
     String name;
-    String rollno;
-    String roomno;
+    String roll_number;
+    String room_number;
     String course;
 
-    public Student(String name, String rollno, String roomno, String course) {
+    public Student(String name , String roll_number , String room_number , String course){
         this.name = name;
-        this.rollno = rollno;
-        this.roomno = roomno;
-        this.course = course;
+        this.roll_number = roll_number;
+        this.room_number =room_number;
+        this.course=course;
     }
-
-    public String getName() {
+    public String getName(){
         return name;
     }
-
-    public String getRollno() {
-        return rollno;
+    public String getRoll_number(){
+        return roll_number;
     }
-
-    public String getRoomno() {
-        return roomno;
+    public String getRoom_number(){
+        return room_number;
     }
-
-    public String getCourse() {
+    public String getCourse(){
         return course;
     }
-
-    public void setRoomno(String roomno) {
-        this.roomno = roomno;
+    
+    public void setRoom_number(String room_number){
+        this.room_number = room_number;
     }
+
 }
 
-class Hostel {
-    Student[] students;
-    int numStudents;
-    int numRooms;
+class Hostel{
+    Student[] students; 
+    int Students_num;
+    int Rooms_num;
     int[] rooms;
 
-    public Hostel(int numStudents, int numRooms) {
-        this.numStudents = numStudents;
-        this.numRooms = numRooms;
-        this.rooms = new int[numRooms];
-        this.students = new Student[numStudents];
+    public Hostel(int Students_num , int Rooms_num){
+        this.Students_num=Students_num;
+        this.Rooms_num=Rooms_num;
+        this.rooms=new int[Rooms_num];
+        this.students=new Student[Students_num];
     }
 
-    public void addStudent(Student s) {
-        for (int i = 0; i < numStudents; i++) {
-            if (students[i] == null) {
-                students[i] = s;
-                System.out.println("Student " + s.getName() + " has been added to the system.");
+    //1st method
+    public void Add_Student(Student s){
+        for(int i=0; i<Students_num;i++){
+            if(students[i]==null){
+                students[i]=s;
+                System.out.println("Student "+ s.getName() + " has been added to the system.");
                 break;
             }
         }
     }
-
-    public void allocateRoom() {
-        int count = 0;
-        for (int i = 0; i < numRooms; i++) {
-            if (rooms[i] == 0) {
+    
+    //2nd method
+    public void Room_Allocation(){
+        int count = 0 ;
+        for(int i=0; i<Rooms_num;i++){
+            if(rooms[i]==0){
                 count++;
             }
         }
-        if (count == 0) {
-            System.out.println("Sorry, there are no rooms available at the moment.");
+        if(count==0){
+            System.out.println("There are no rooms available, Have a nice day!");
             return;
         }
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the roll number of the student you want to allocate a room to: ");
-        String rollno = sc.nextLine();
-        for (int i = 0; i < numStudents; i++) {
-            if (students[i].getRollno().equals(rollno)) {
-                for (int j = 0; j < numRooms; j++) {
-                    if (rooms[j] == 0) {
-                        rooms[j] = 1;
-                        students[i].setRoomno(Integer.toString(j+1));
-                        System.out.println("Room " + (j+1) + " has been allocated to " + students[i].getName() + ".");
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the roll number of the student who is ready to choose the room : ");
+        String roll_number = s.nextLine();
+        for(int i=0 ; i<Students_num;i++){
+            if(students[i].getRoll_number().equals(roll_number)){
+                for(int j=0; j<Rooms_num;j++){
+                    if(rooms[j]==0){
+                        rooms[j]=1;
+                        students[i].setRoom_number(Integer.toString(j+1));
+                        System.out.println("Congrats room number " + (j+1) + " has been allocated to "+ students[i].getName());
                         return;
                     }
                 }
             }
         }
-        System.out.println("Sorry, the student with roll number " + rollno + " is not registered.");
+        System.out.println("Sorry the student wiht roll number "+roll_number+ " is not registered.");
+
     }
 
-    public void viewStudentDetails() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the roll number of the student: ");
-        String rollno = sc.nextLine();
-        for (int i = 0; i < numStudents; i++) {
-            if (students[i].getRollno().equals(rollno)) {
-                System.out.println("Name: " + students[i].getName());
-                System.out.println("Roll number: " + students[i].getRollno());
-                System.out.println("Course: " + students[i].getCourse());
-                System.out.println("Room number: " + students[i].getRoomno());
+
+    //3rd method
+    public void Student_Details(){
+        Scanner s= new Scanner(System.in);
+        System.out.println("Please, enter the roll number of the student: ");
+        String roll_number =s.nextLine();
+        for(int i=0; i<Students_num;i++){
+            if(students[i].getRoll_number().equals(roll_number)){
+                System.out.println("Name : "+ students[i].getName());
+                System.out.println("Roll-Number :"+ students[i].getRoll_number());
+                System.out.println("Room-Number :"+ students[i].getRoom_number());
+                System.out.println("Course :" + students[i].getCourse());
                 return;
             }
         }
-        System.out.println("Sorry, the student with roll number " + rollno + " is not registered.");
+        System.out.println("Please look again, the student with the roll number "+roll_number+ " is not been registered.");
     }
+
+
+
+
 }
 
-public class HostelManagementSystem {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of students: ");
-        int numStudents = sc.nextInt();
-        System.out.println("Enter the number of rooms: ");
-        int numRooms = sc.nextInt();
-        Hostel hostel = new Hostel(numStudents, numRooms);
-        while (true) {
-            System.out.println("Enter 1 to add a student.");
-            System.out.println("Enter 2 to allocate a room.");
-            System.out.println("Enter 3 to view student details.");
-            System.out.println("Enter 4 to exit.");
-            int choice = sc.nextInt();
-            switch (choice) {
+public class HMS{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter total number of students : ");
+        int Students_num=s.nextInt();
+        System.out.println("Enter the number of available rooms : ");
+        int Rooms_num=s.nextInt();
+        Hostel hostel=new Hostel(Students_num,Rooms_num);
+        while(true){
+            System.out.println("Enter 1 to add a student");
+            System.out.println("Enter 2 to allocate a room");
+            System.out.println("Enter 3 to view student details");
+            System.out.println("Enter 4 to exit");
+            int choice =s.nextInt();
+            switch(choice){
                 case 1:
-                    System.out.println("Enter the name of the student: ");
-                    String name = sc.next();
-                    System.out.println("Enter the roll number of the student: ");
-                    String rollno = sc.next();
-                    System.out.println("Enter the course of the student: ");
-                    String course = sc.next();
-                    Student s = new Student(name, rollno, null, course);
-                    hostel.addStudent(s);
-                    break;
+                System.out.println("Enter the name of the student: ");
+                String name =s.next();
+                System.out.println("Enter the roll number of the student : ");
+                String roll_number=s.next();
+                System.out.println("Enter the enrolled course of the student :");
+                String course = s.next();
+                Student sm = new Student(name , roll_number , null , course);
+                hostel.Add_Student(sm);
+                break;
                 case 2:
-                    hostel.allocateRoom();
-                    break;
+                hostel.Room_Allocation();
+                break;
                 case 3:
-                    hostel.viewStudentDetails();
-                    break;
+                hostel.Student_Details();
+                break;
                 case 4:
-                    System.exit(0);
-                    break;
+                System.exit(0);
+                break;
                 default:
-                    System.out.println("Invalid choice.");
+                System.out.println("Invalid input.");
             }
         }
     }
